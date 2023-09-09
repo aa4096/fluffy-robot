9ecf0b96b78ec1c651facd14d42e9f6289fa6886
aa2048 <apena89@gmail.com>
2023-09-09 13:49:19 -0500

    Update file, fix some bugs

--- run.py
+++ run.py
@@ -38,7 +38,13 @@ print("\n\n~~~ Welcome to my Python File System Simulator ~~\n\n")
 
 while True:
   command = input("Choose a command (ls/cd/mkdir/touch): ")
+  target_directory = "root"
 
+  # Invalid command
+  if command != "cd" and command != "ls" and command != "mkdir" and command != "touch":
+    print("Not a valid command. Try again.")
+
+  # Change directory
   if command == "cd":
     target_directory = input("Enter a directory: ")
     directory_found = False
@@ -54,14 +60,35 @@ while True:
         if target_directory in directory:
           directory_found = True
           previous_directory = current_directory
-          current_directory = directory[target_directory]
-          print(f"\nHere are the items in {target_directory}:")
+          try:
+            current_directory = directory[target_directory]
+          except KeyError as e:
+            print(f"There was an error. Try again.")
+            continue
+          print(f"\nHere are the items in {target_directory}:\n")
           print(TransformObjectToList(current_directory))
       
     if directory_found == False:
-      print(f"\"{target_directory}\" does not exist. Try again.")
+      print(f"\n\"{target_directory}\" does not exist. Try again.\n")
 
+  # List files and directories
   if command == "ls":
     print(f"Here are the items in the current directory:\n")
     print(f"\t../")
+    print(TransformObjectToList(current_directory))
+
+  # Make directory
+  if command == "mkdir":
+    target_directory = input("Enter a directory name: ")
+
+    current_directory.append({target_directory:[]})
+    print("")
+    print(TransformObjectToList(current_directory))
+
+  # Touch
+  if command == "touch":
+    target_file = input("Enter a file name: ")
+
+    current_directory.append(target_file)
+    print(f"\nHere are the items in this directory:\n")
     print(TransformObjectToList(current_directory))
